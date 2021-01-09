1cf1150105524b61123a23e6043561ca40976aec
Pardeep <pardeepcitm@gmail.com>
2021-01-09 11:54:17 +0000

    second change

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -12,9 +12,9 @@ namespace Catalog.Repositories
         private readonly List<Item> items = new()
         {
 
-            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
-            new Item { Id = Guid.NewGuid(), Name = "Totion", Price = 10, CreatedDate = DateTimeOffset.UtcNow },
-            new Item { Id = Guid.NewGuid(), Name = "Motion", Price = 11, CreatedDate = DateTimeOffset.UtcNow }
+            new Item { Id = Guid.NewGuid(), Name = "New", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Test", Price = 10, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "App", Price = 11, CreatedDate = DateTimeOffset.UtcNow }
         };
 
         public async Task<IEnumerable<Item>> GetItemsAsync()
